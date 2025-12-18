d0658e508b75907b80389f9616d95a3c81cfc9cf
Maira <maira212.a@gmail.com>
2025-12-18 11:47:15 +0000

    Modelo de Datos de Instagram

--- src/models.py
+++ src/models.py
@@ -1,10 +1,11 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
+from sqlalchemy import String, Boolean, ForeignKey, Integer
 from sqlalchemy.orm import Mapped, mapped_column
 
 db = SQLAlchemy()
 
 class User(db.Model):
+    __tablename__ = 'user'
     id: Mapped[int] = mapped_column(primary_key=True)
     email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
@@ -17,3 +18,27 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+# class Follow(db.Model):
+#     __tablename__ = 'follow'
+#     user_from_id: Mapped[int] = mapped_column(ForeignKey('user.id')) 
+#     user_to_id: Mapped[int] = mapped_column(ForeignKey('user.id'))
+
+class Media(db.Model):
+    __tablename__ = 'media'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    type: Mapped[int] = mapped_column(Integer)
+    url: Mapped[str] = mapped_column(String, unique=True, nullable=False)
+    post_id: Mapped[int] = mapped_column(ForeignKey('post.id'))
+
+class Post(db.Model):
+    __tablename__ = 'post'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey('user.id'))
+
+class Comment(db.Model):
+    __tablename__ = 'comment'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    comment_text: Mapped[str] = mapped_column(String(400))
+    author_id: Mapped[int] = mapped_column(ForeignKey('user.id'))
+    post_id: Mapped[int] = mapped_column(ForeignKey('post.id'))
